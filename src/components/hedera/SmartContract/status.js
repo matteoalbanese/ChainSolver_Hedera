9c5f1e28b953a61727d4a17f23aa178497c53c77
matteoalbanese <134776661+matteoalbanese@users.noreply.github.com>
2023-06-20 23:54:48 +0200

    Create status.js

---
+++ src/components/hedera/SmartContract/status.js
@@ -0,0 +1,32 @@
+import abi from "../../../contracts/abi.js";
+import { ethers } from "ethers";
+
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+async function getStatus(walletData,contractAddress) {
+
+    console.log(`\n=======================================`);
+    
+    const provider = walletData[1];
+    const signer = provider.getSigner();
+    let value;
+
+    
+    try{
+
+        const myContract = new ethers.Contract(contractAddress, abi, signer);
+        
+        const getStatusTx = await myContract.state.call();
+
+        value = getStatusTx;
+
+        await delay(500);
+        
+    }catch(getStatusError){
+        console.log(`- ${getStatusError.message.toString()}`);
+    }
+
+    return [value];
+}
+export default getStatus;
+
