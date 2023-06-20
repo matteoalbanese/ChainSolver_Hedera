444e069a02f42a93ba504db5d5b40bd040af1652
matteoalbanese <134776661+matteoalbanese@users.noreply.github.com>
2023-06-20 23:52:57 +0200

    Create confirmPurchase.js

---
+++ src/components/hedera/SmartContract/confirmPurchase.js
@@ -0,0 +1,35 @@
+import abi from "../../../contracts/abi.js";
+import { ethers } from "ethers";
+
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+async function confirmPurchaseFcn(walletData, contractAddress, prezzoX2){
+
+    console.log(`\n=======================================`);
+
+    const provider = walletData[1];
+
+    const signer = provider.getSigner();
+
+    let txHash;
+
+    try{
+        
+        const myContract = new ethers.Contract(contractAddress, abi, signer);
+        
+        const confirmPurchaseTx = await myContract.confirmPurchase( {value: ethers.utils.parseEther(prezzoX2.toString())});
+        const confirmPurchaseRx = await confirmPurchaseTx.wait();
+        
+        txHash = confirmPurchaseRx.transactionHash;
+
+        await delay(500);
+        
+        console.log("payment confirmed");
+        console.log(`transaction hash: \n${txHash}`);
+
+    }catch(confirmPurchaseError){
+        console.log(`${confirmPurchaseError.message.toString()}`);
+    }
+}
+
+export default confirmPurchaseFcn;
