471bcffe70fe319eae20dcf5526f83bc07c3b080
rebeccamammone2 <135345382+rebeccamammone2@users.noreply.github.com>
2023-06-21 17:17:52 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Worker from './pages/app_worker';
 import Navbar from './pages/Navbar';
 import Login from './pages/login';
 import Signup from './pages/signup';
+import SignupFreelancer from './pages/signupFreelancer';
+import SignupClient from './pages/signupClient';
+
 
 function App() {
   return (
@@ -13,19 +16,21 @@ function App() {
       <Routes>
           
           <Route exact path='/app_buyer' element={<Buyer/>}>
-           
           </Route>
           
           <Route exact path='/app_worker' element={<Worker/>}>
-            
           </Route>
 
           <Route exact path='/login' element={<Login/>}>
-            
           </Route>
 
           <Route exact path='/signup' element={<Signup/>}>
-            
+          </Route>
+
+          <Route exact path='/signupFreelancer' element={<SignupFreelancer/>}>
+          </Route>
+
+          <Route exact path='/signupClient' element={<SignupClient/>}>
           </Route>
          
       </Routes>
