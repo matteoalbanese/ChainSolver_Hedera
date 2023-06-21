13b33d6e2664840dccdfe216a27a9b460f07aeb4
rebeccamammone2 <135345382+rebeccamammone2@users.noreply.github.com>
2023-06-21 17:19:10 +0200

    Add files via upload

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -2,24 +2,47 @@ import React from 'react';
 import '../styles/signup.css';
 
 function Signup() {
+
+
+    const handleClient=(data)=>{
+        document.querySelector('#freelancer').classList.remove('verde');
+        document.querySelector('#freelancer input').checked = false;
+        document.querySelector('#client').classList.add('verde');
+        document.querySelector('button').classList.add('vbottone');
+        document.querySelector('button').textContent="Join as a Client";
+        document.querySelector('#bottone').href="signupClient";
+    }
+
+    const handleFreelancer=(data)=>{
+        document.querySelector('#client').classList.remove('verde');
+        document.querySelector('#client input').checked=false;
+        document.querySelector('#freelancer').classList.add('verde');
+        document.querySelector('button').classList.add('vbottone');
+        document.querySelector('button').textContent="Apply as a Freelancer";
+        document.querySelector('#bottone').href="signupFreelancer";
+    }
+
+
+    
+
     return (
       <div>
         <div className="background">
-            <div className="shape"></div>
-            <div className="shape"></div>
+            <div className="shape2"></div>
+            <div className="shape2"></div>
         </div>
 
-        <div className="section">
+        <div className="section1">
             
         <form name='signup' method='post' enctype="multipart/form-data" autocomplete="off">
             <h3>Join as a client or freelancer </h3>
             <div id="client">
-                <input type='checkbox' name='client' value="1" id="client" />
+                <input type='checkbox' name='client' value="client" id="client" onChange={()=>handleClient("client")}/>
                 <label for='client'>I'm a client, hiring for a project</label>
             </div>
 
             <div id="freelancer">
-                <input type='checkbox' name='freelancer' value="1" id="freelancer" />
+                <input type='checkbox' name='freelancer' value="freelancer" id="freelancer" onChange={()=>handleFreelancer("freelancer")}/>
                 <label for='freelancer'>I'm a freelancer, looking for a work</label>
             </div>
         </form>
