8f3970a9e1c7a3574f6b120df5be9f5539bada90
rebeccamammone2 <135345382+rebeccamammone2@users.noreply.github.com>
2023-06-21 17:18:32 +0200

    Update Navbar.jsx

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -4,25 +4,16 @@ import {Link} from 'react-router-dom'
 
 function Navbar() {
     return (
+        
             <div className='navbar'>
             
                 <div className='navbar_list'>
                     <ul>
-                        <li>
-                            <Link to="/app_buyer">Buyer</Link>
-                        </li>
-
-                        <li>
-                            <Link to="/app_worker">Worker</Link>
-                        </li>
-
+                        
                         <li>
                             <Link to="/login">Login</Link>
                         </li>
 
-                        <li>
-                            <Link to="/signup">Signup</Link>
-                        </li>
                         
                     </ul>
                 </div>
