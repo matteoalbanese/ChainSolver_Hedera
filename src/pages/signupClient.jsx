1908d987a6e6cada57766ac57f17ffaeb413f268
matteoalbanese <134776661+matteoalbanese@users.noreply.github.com>
2023-06-23 13:05:59 +0200

    Update signupClient.jsx

--- src/pages/signupClient.jsx
+++ src/pages/signupClient.jsx
@@ -18,42 +18,36 @@ function SignupClient() {
         <div className="firstname">
         <label for="firstname">First Name</label>
         <input type="text" placeholder="First Name" id="firstname"/> 
-        <span> Devi inserire il tuo nome </span>
         </div>
 
         <div className="lastname">
         <label for="lastname">Last Name</label>
         <input type="text" placeholder="Last Name" id="lastname"/>
-        <span> Devi inserire il tuo cognome </span>
         </div>
 
         <div className="username">
         <label for="username">Username</label>
         <input type="text" placeholder="Email or Phone" id="username"/> 
-        <span> Nome utente non disponibile </span>
         </div>
 
         <div className="email">
         <label for="email">Email</label>
         <input type="text" placeholder="Email" id="email"/>
-        <span> Indirizzo email non valida </span>
         </div>
 
         <div className="password">
         <label for="password">Password</label>
         <input type="password" placeholder="Password" id="password"/> 
-        <span> Inserisci almeno 8 caratteri </span>
         </div>
 
         <div className="confirm_password">
         <label for="confirm_password">Confirm Password</label>
         <input type="text" placeholder="Confirm Password" id="confirm_password"/>
-        <span> Le password non corrispondono </span>
         </div>
 
         <div id="allow">
         <input type='checkbox' name='allow' value="1" id="allow" />
-        <label for='allow'>Accetto i termini e condizioni d'uso di ChainSolver.</label>
+        <label for='allow'>Agree to Terms and Conditions</label>
         </div>
 
         <div className="submit">
